c9464e5c8e04aff9bc6793cad2e76f3d40b8927b
Juan Rosario <juanelias18@hotmail.com>
2019-06-27 21:04:17 -0400

    Contexto establecido

---
+++ Parcial2-JuanElias/DAL/Contexto.cs
@@ -0,0 +1,18 @@
+﻿using Parcial2_JuanElias.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial2_JuanElias.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Asignaturas> Asignatura { get; set; }
+        public DbSet<Estudiantes> Estudiante { get; set; }
+        public DbSet<Inscripciones> Inscripcion { get; set; }
+        public Contexto() : base("ConStr") { }
+    }
+}
