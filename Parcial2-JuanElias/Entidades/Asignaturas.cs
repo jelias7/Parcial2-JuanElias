3eaa33955b6e5e3cb653bece444ff3bb0241c946
Juan Rosario <juanelias18@hotmail.com>
2019-06-27 20:44:10 -0400

    Entidad de Asignaturas hecha.

---
+++ Parcial2-JuanElias/Entidades/Asignaturas.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial2_JuanElias.Entidades
+{
+    public class Asignaturas
+    {
+        [Key]
+        public int AsignaturaId { get; set; }
+        public string Descripcion { get; set; }
+        public double Creditos { get; set; }
+        public Asignaturas()
+        {
+            AsignaturaId = 0;
+            Descripcion = string.Empty;
+            Creditos = 0;
+        }
+    }
+}
