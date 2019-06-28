3d52d99a3e42597ee307ed5702d17e471ef3d826
Juan Rosario <juanelias18@hotmail.com>
2019-06-27 20:47:13 -0400

    Entidad de Estudiantes hecha.

---
+++ Parcial2-JuanElias/Entidades/Estudiantes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial2_JuanElias.Entidades
+{
+    public class Estudiantes
+    {
+        [Key]
+        public int EstudianteId { get; set; }
+        public DateTime FechaIngreso { get; set; }
+        public string Nombres { get; set; }
+        public double Balance { get; set; }
+        public Estudiantes()
+        {
+            EstudianteId = 0;
+            FechaIngreso = DateTime.Now;
+            Nombres = string.Empty;
+            Balance = 0;
+        }
+    }
+}
