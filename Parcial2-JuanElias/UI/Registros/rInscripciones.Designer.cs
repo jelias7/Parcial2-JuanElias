9a582bf48f06fd3ea788512253ab90e9a6f8a364
Juan Rosario <juanelias18@hotmail.com>
2019-06-29 18:26:56 -0400

    Registro de Inscripciones diseñado

---
+++ Parcial2-JuanElias/UI/Registros/rInscripciones.Designer.cs
@@ -0,0 +1,311 @@
+﻿namespace Parcial2_JuanElias.UI.Registros
+{
+    partial class rInscripciones
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.InscripcionIdnumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.EstudiantecomboBox = new System.Windows.Forms.ComboBox();
+            this.CostoCreditosnumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.TotalnumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.AsignaturacomboBox = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.FechadateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.DetalledataGridView = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.Removerbutton = new System.Windows.Forms.Button();
+            this.Addbutton = new System.Windows.Forms.Button();
+            this.Eliminarbutton = new System.Windows.Forms.Button();
+            this.Guardarbutton = new System.Windows.Forms.Button();
+            this.Nuevobutton = new System.Windows.Forms.Button();
+            this.Buscarbutton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.InscripcionIdnumericUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CostoCreditosnumericUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TotalnumericUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DetalledataGridView)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "InscripcionId";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Estudiante";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Fecha";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 150);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Costo Creditos";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(233, 410);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(31, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Total";
+            // 
+            // InscripcionIdnumericUpDown
+            // 
+            this.InscripcionIdnumericUpDown.Location = new System.Drawing.Point(100, 23);
+            this.InscripcionIdnumericUpDown.Name = "InscripcionIdnumericUpDown";
+            this.InscripcionIdnumericUpDown.Size = new System.Drawing.Size(71, 20);
+            this.InscripcionIdnumericUpDown.TabIndex = 5;
+            // 
+            // EstudiantecomboBox
+            // 
+            this.EstudiantecomboBox.FormattingEnabled = true;
+            this.EstudiantecomboBox.Location = new System.Drawing.Point(100, 63);
+            this.EstudiantecomboBox.Name = "EstudiantecomboBox";
+            this.EstudiantecomboBox.Size = new System.Drawing.Size(216, 21);
+            this.EstudiantecomboBox.TabIndex = 6;
+            // 
+            // CostoCreditosnumericUpDown
+            // 
+            this.CostoCreditosnumericUpDown.DecimalPlaces = 2;
+            this.CostoCreditosnumericUpDown.Location = new System.Drawing.Point(100, 150);
+            this.CostoCreditosnumericUpDown.Maximum = new decimal(new int[] {
+            2000,
+            0,
+            0,
+            0});
+            this.CostoCreditosnumericUpDown.Name = "CostoCreditosnumericUpDown";
+            this.CostoCreditosnumericUpDown.Size = new System.Drawing.Size(120, 20);
+            this.CostoCreditosnumericUpDown.TabIndex = 7;
+            // 
+            // TotalnumericUpDown
+            // 
+            this.TotalnumericUpDown.DecimalPlaces = 2;
+            this.TotalnumericUpDown.Location = new System.Drawing.Point(287, 410);
+            this.TotalnumericUpDown.Maximum = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            this.TotalnumericUpDown.Name = "TotalnumericUpDown";
+            this.TotalnumericUpDown.Size = new System.Drawing.Size(120, 20);
+            this.TotalnumericUpDown.TabIndex = 8;
+            // 
+            // AsignaturacomboBox
+            // 
+            this.AsignaturacomboBox.FormattingEnabled = true;
+            this.AsignaturacomboBox.Location = new System.Drawing.Point(88, 26);
+            this.AsignaturacomboBox.Name = "AsignaturacomboBox";
+            this.AsignaturacomboBox.Size = new System.Drawing.Size(262, 24);
+            this.AsignaturacomboBox.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(25, 26);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(57, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Asignatura";
+            // 
+            // FechadateTimePicker
+            // 
+            this.FechadateTimePicker.CustomFormat = "dd/MM/yyyy";
+            this.FechadateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.FechadateTimePicker.Location = new System.Drawing.Point(100, 106);
+            this.FechadateTimePicker.Name = "FechadateTimePicker";
+            this.FechadateTimePicker.Size = new System.Drawing.Size(121, 20);
+            this.FechadateTimePicker.TabIndex = 11;
+            // 
+            // DetalledataGridView
+            // 
+            this.DetalledataGridView.AllowUserToAddRows = false;
+            this.DetalledataGridView.AllowUserToDeleteRows = false;
+            this.DetalledataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DetalledataGridView.Location = new System.Drawing.Point(19, 254);
+            this.DetalledataGridView.Name = "DetalledataGridView";
+            this.DetalledataGridView.Size = new System.Drawing.Size(388, 150);
+            this.DetalledataGridView.TabIndex = 12;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Addbutton);
+            this.groupBox1.Controls.Add(this.Removerbutton);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.AsignaturacomboBox);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 198);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(404, 254);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Info.";
+            // 
+            // Removerbutton
+            // 
+            this.Removerbutton.Font = new System.Drawing.Font("Arial Narrow", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Removerbutton.Image = global::Parcial2_JuanElias.Properties.Resources.delete_delete_exit_1577;
+            this.Removerbutton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Removerbutton.Location = new System.Drawing.Point(7, 212);
+            this.Removerbutton.Name = "Removerbutton";
+            this.Removerbutton.Size = new System.Drawing.Size(103, 36);
+            this.Removerbutton.TabIndex = 14;
+            this.Removerbutton.Text = "REMOVER";
+            this.Removerbutton.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.Removerbutton.UseVisualStyleBackColor = true;
+            // 
+            // Addbutton
+            // 
+            this.Addbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Addbutton.Image = global::Parcial2_JuanElias.Properties.Resources.add_insert_plus_1588;
+            this.Addbutton.Location = new System.Drawing.Point(356, 13);
+            this.Addbutton.Name = "Addbutton";
+            this.Addbutton.Size = new System.Drawing.Size(39, 39);
+            this.Addbutton.TabIndex = 14;
+            this.Addbutton.UseVisualStyleBackColor = true;
+            // 
+            // Eliminarbutton
+            // 
+            this.Eliminarbutton.Image = global::Parcial2_JuanElias.Properties.Resources.delete_delete_exit_1577;
+            this.Eliminarbutton.Location = new System.Drawing.Point(343, 458);
+            this.Eliminarbutton.Name = "Eliminarbutton";
+            this.Eliminarbutton.Size = new System.Drawing.Size(64, 47);
+            this.Eliminarbutton.TabIndex = 14;
+            this.Eliminarbutton.UseVisualStyleBackColor = true;
+            // 
+            // Guardarbutton
+            // 
+            this.Guardarbutton.Image = global::Parcial2_JuanElias.Properties.Resources.check_ok_accept_apply_1582;
+            this.Guardarbutton.Location = new System.Drawing.Point(181, 458);
+            this.Guardarbutton.Name = "Guardarbutton";
+            this.Guardarbutton.Size = new System.Drawing.Size(62, 47);
+            this.Guardarbutton.TabIndex = 15;
+            this.Guardarbutton.UseVisualStyleBackColor = true;
+            // 
+            // Nuevobutton
+            // 
+            this.Nuevobutton.Image = global::Parcial2_JuanElias.Properties.Resources.add_insert_plus_1588;
+            this.Nuevobutton.Location = new System.Drawing.Point(19, 458);
+            this.Nuevobutton.Name = "Nuevobutton";
+            this.Nuevobutton.Size = new System.Drawing.Size(61, 47);
+            this.Nuevobutton.TabIndex = 16;
+            this.Nuevobutton.UseVisualStyleBackColor = true;
+            // 
+            // Buscarbutton
+            // 
+            this.Buscarbutton.Image = global::Parcial2_JuanElias.Properties.Resources.Search_find_locate_1542;
+            this.Buscarbutton.Location = new System.Drawing.Point(181, 12);
+            this.Buscarbutton.Name = "Buscarbutton";
+            this.Buscarbutton.Size = new System.Drawing.Size(40, 37);
+            this.Buscarbutton.TabIndex = 17;
+            this.Buscarbutton.UseVisualStyleBackColor = true;
+            // 
+            // rInscripciones
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(428, 512);
+            this.Controls.Add(this.Buscarbutton);
+            this.Controls.Add(this.Nuevobutton);
+            this.Controls.Add(this.Guardarbutton);
+            this.Controls.Add(this.Eliminarbutton);
+            this.Controls.Add(this.DetalledataGridView);
+            this.Controls.Add(this.FechadateTimePicker);
+            this.Controls.Add(this.TotalnumericUpDown);
+            this.Controls.Add(this.CostoCreditosnumericUpDown);
+            this.Controls.Add(this.EstudiantecomboBox);
+            this.Controls.Add(this.InscripcionIdnumericUpDown);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "rInscripciones";
+            this.Text = "Registro de Inscripciones";
+            ((System.ComponentModel.ISupportInitialize)(this.InscripcionIdnumericUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CostoCreditosnumericUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TotalnumericUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DetalledataGridView)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown InscripcionIdnumericUpDown;
+        private System.Windows.Forms.ComboBox EstudiantecomboBox;
+        private System.Windows.Forms.NumericUpDown CostoCreditosnumericUpDown;
+        private System.Windows.Forms.NumericUpDown TotalnumericUpDown;
+        private System.Windows.Forms.ComboBox AsignaturacomboBox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DateTimePicker FechadateTimePicker;
+        private System.Windows.Forms.DataGridView DetalledataGridView;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button Addbutton;
+        private System.Windows.Forms.Button Removerbutton;
+        private System.Windows.Forms.Button Eliminarbutton;
+        private System.Windows.Forms.Button Guardarbutton;
+        private System.Windows.Forms.Button Nuevobutton;
+        private System.Windows.Forms.Button Buscarbutton;
+    }
+}
