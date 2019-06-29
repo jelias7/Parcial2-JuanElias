52dda36eda68142ab11ecf52722e60818c462671
Juan Rosario <juanelias18@hotmail.com>
2019-06-28 20:33:47 -0400

    Entidad del detalle agregado

---
+++ Parcial2-JuanElias/Entidades/InscripcionesDetalle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial2_JuanElias.Entidades
+{
+    public class InscripcionesDetalle
+    {
+        [Key]
+        public int DetalleId { get; set; }
+        public int InscripcionId { get; set; }
+        public int AsignaturaId { get; set; }
+        public double Subtotal { get; set; }
+        public InscripcionesDetalle()
+        {
+            DetalleId = 0;
+            InscripcionId = 0;
+            AsignaturaId = 0;
+            Subtotal = 0;
+        }
+    }
+}
