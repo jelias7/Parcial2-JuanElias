ee06b6a942eb1ac1c683f275e0ee14e4ff673b05
Juan Rosario <juanelias18@hotmail.com>
2019-06-30 15:54:27 -0400

    Avanzando en la consulta

--- Parcial2-JuanElias/UI/Consultas/cEstudiantes.cs
+++ Parcial2-JuanElias/UI/Consultas/cEstudiantes.cs
@@ -31,11 +31,61 @@ namespace Parcial2_JuanElias.UI.Consultas
                     case "Todo":
                         listado = r.GetList(p => true);
                         break;
+                    case "ID":
+                        int parse;
+                        if (!int.TryParse(CriteriotextBox.Text, out parse))
+                        {
+                            MessageBox.Show("Solo numeros.");
+                        }
+                        else
+                        {
+                            int id = Convert.ToInt32(CriteriotextBox.Text);
+                            listado = r.GetList(p => p.EstudianteId == id);
+                        }
+                        break;
+                    case "Nombres":
+                        if(!System.Text.RegularExpressions.Regex.IsMatch(CriteriotextBox.Text, "^[a-zA-Z ]"))
+                        {
+                            MessageBox.Show("No numeros en los nombres.");
+                        }
+                        else
+                        {
+                            listado = r.GetList(p => p.Nombres.Contains(CriteriotextBox.Text));
+                        }
+                        break;
+
+                    case "Balance":
+                        double parsec;
+                        if (!double.TryParse(CriteriotextBox.Text, out parsec))
+                        {
+                            MessageBox.Show("Solo numeros.");
+                        }
+                        else
+                        {
+                            double c = double.Parse(CriteriotextBox.Text);
+                            listado = r.GetList(p => p.Balance == c);
+                        }
+                        break;
                 }
             }
             else
             {
-                listado = r.GetList(p => true);
+                if (FiltrocomboBox.Text == string.Empty)
+                {
+                    MessageBox.Show("El filtro no puede estar vacio.");
+                }
+                else
+                    if ((string)FiltrocomboBox.Text != "Todo")
+                {
+                    if (CriteriotextBox.Text == string.Empty)
+                    {
+                        MessageBox.Show("Al seleccionar uno de ID, Nombres o Balance necesita escribir algo en el criterio.");
+                    }
+                }
+                else
+                {
+                    listado = r.GetList(p => true);
+                }
             }
             ConsultadataGridView.DataSource = null;
             ConsultadataGridView.DataSource = listado;
