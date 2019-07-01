9cecd87de3cbd747b22ea1cddeb06325b3fd79d9
Juan Rosario <juanelias18@hotmail.com>
2019-06-30 23:03:12 -0400

    Comenzando con Consulta de Inscripciones

---
+++ Parcial2-JuanElias/UI/Consultas/cInscripciones.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace Parcial2_JuanElias.UI.Consultas
+{
+    partial class cInscripciones
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.RangocheckBox = new System.Windows.Forms.CheckBox();
+            this.ConsultadataGridView = new System.Windows.Forms.DataGridView();
+            this.HastadateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.DesdedateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.CriteriotextBox = new System.Windows.Forms.TextBox();
+            this.FiltrocomboBox = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Consultabutton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.ConsultadataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // RangocheckBox
+            // 
+            this.RangocheckBox.AutoSize = true;
+            this.RangocheckBox.Location = new System.Drawing.Point(251, 44);
+            this.RangocheckBox.Name = "RangocheckBox";
+            this.RangocheckBox.Size = new System.Drawing.Size(107, 17);
+            this.RangocheckBox.TabIndex = 21;
+            this.RangocheckBox.Text = "Buscar por fecha";
+            this.RangocheckBox.UseVisualStyleBackColor = true;
+            // 
+            // ConsultadataGridView
+            // 
+            this.ConsultadataGridView.AllowUserToAddRows = false;
+            this.ConsultadataGridView.AllowUserToDeleteRows = false;
+            this.ConsultadataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.ConsultadataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ConsultadataGridView.Location = new System.Drawing.Point(23, 68);
+            this.ConsultadataGridView.Name = "ConsultadataGridView";
+            this.ConsultadataGridView.ReadOnly = true;
+            this.ConsultadataGridView.Size = new System.Drawing.Size(736, 372);
+            this.ConsultadataGridView.TabIndex = 20;
+            // 
+            // HastadateTimePicker
+            // 
+            this.HastadateTimePicker.CustomFormat = "dd/MM/yyyy";
+            this.HastadateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.HastadateTimePicker.Location = new System.Drawing.Point(148, 42);
+            this.HastadateTimePicker.Name = "HastadateTimePicker";
+            this.HastadateTimePicker.Size = new System.Drawing.Size(97, 20);
+            this.HastadateTimePicker.TabIndex = 19;
+            // 
+            // DesdedateTimePicker
+            // 
+            this.DesdedateTimePicker.CustomFormat = "dd/MM/yyyy";
+            this.DesdedateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.DesdedateTimePicker.Location = new System.Drawing.Point(23, 42);
+            this.DesdedateTimePicker.Name = "DesdedateTimePicker";
+            this.DesdedateTimePicker.Size = new System.Drawing.Size(99, 20);
+            this.DesdedateTimePicker.TabIndex = 18;
+            // 
+            // CriteriotextBox
+            // 
+            this.CriteriotextBox.Location = new System.Drawing.Point(520, 42);
+            this.CriteriotextBox.Name = "CriteriotextBox";
+            this.CriteriotextBox.Size = new System.Drawing.Size(190, 20);
+            this.CriteriotextBox.TabIndex = 17;
+            // 
+            // FiltrocomboBox
+            // 
+            this.FiltrocomboBox.FormattingEnabled = true;
+            this.FiltrocomboBox.Items.AddRange(new object[] {
+            "InscripcionId",
+            "EstudianteId",
+            "Fecha",
+            "Monto",
+            "Monto de Creditos"});
+            this.FiltrocomboBox.Location = new System.Drawing.Point(364, 42);
+            this.FiltrocomboBox.Name = "FiltrocomboBox";
+            this.FiltrocomboBox.Size = new System.Drawing.Size(150, 21);
+            this.FiltrocomboBox.TabIndex = 16;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(517, 11);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Criterio";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(361, 11);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Filtro";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(145, 11);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Hasta";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Desde";
+            // 
+            // Consultabutton
+            // 
+            this.Consultabutton.Image = global::Parcial2_JuanElias.Properties.Resources.Search_find_locate_1542;
+            this.Consultabutton.Location = new System.Drawing.Point(716, 15);
+            this.Consultabutton.Name = "Consultabutton";
+            this.Consultabutton.Size = new System.Drawing.Size(43, 48);
+            this.Consultabutton.TabIndex = 11;
+            this.Consultabutton.UseVisualStyleBackColor = true;
+            // 
+            // cInscripciones
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(775, 450);
+            this.Controls.Add(this.RangocheckBox);
+            this.Controls.Add(this.ConsultadataGridView);
+            this.Controls.Add(this.HastadateTimePicker);
+            this.Controls.Add(this.DesdedateTimePicker);
+            this.Controls.Add(this.CriteriotextBox);
+            this.Controls.Add(this.FiltrocomboBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Consultabutton);
+            this.Name = "cInscripciones";
+            this.Text = "Consulta de Inscripciones";
+            ((System.ComponentModel.ISupportInitialize)(this.ConsultadataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox RangocheckBox;
+        private System.Windows.Forms.DataGridView ConsultadataGridView;
+        private System.Windows.Forms.DateTimePicker HastadateTimePicker;
+        private System.Windows.Forms.DateTimePicker DesdedateTimePicker;
+        private System.Windows.Forms.TextBox CriteriotextBox;
+        private System.Windows.Forms.ComboBox FiltrocomboBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Consultabutton;
+    }
+}
