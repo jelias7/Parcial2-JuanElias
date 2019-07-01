6cb2415e85f47a11e9b503d46829c1e0b9e7efe8
Juan Rosario <juanelias18@hotmail.com>
2019-06-30 23:21:20 -0400

    Consulta de Estudiantes completada

--- Parcial2-JuanElias/UI/Consultas/cEstudiantes.cs
+++ Parcial2-JuanElias/UI/Consultas/cEstudiantes.cs
@@ -23,10 +23,12 @@ namespace Parcial2_JuanElias.UI.Consultas
         {
             var listado = new List<Estudiantes>();
             RepositorioBase<Estudiantes> r = new RepositorioBase<Estudiantes>();
-            if (CriteriotextBox.Text.Trim().Length > 0)
+            if(RangocheckBox.Checked == true)
             {
-                switch (FiltrocomboBox.Text)
+                if (CriteriotextBox.Text.Trim().Length > 0)
                 {
+                    switch (FiltrocomboBox.Text)
+                    {
                         case "Todo":
                             listado = r.GetList(p => true);
                             break;
@@ -65,30 +67,98 @@ namespace Parcial2_JuanElias.UI.Consultas
                                 listado = r.GetList(p => p.Balance == c);
                             }
                             break;
-                }
-            }
-            else
-            {
-                if (FiltrocomboBox.Text == string.Empty)
-                {
-                    MessageBox.Show("El filtro no puede estar vacio.");
+                    }
+                    listado = listado.Where(c => c.FechaIngreso.Date >= DesdedateTimePicker.Value.Date && c.FechaIngreso.Date <= HastadateTimePicker.Value.Date).ToList();
                 }
                 else
-                    if ((string)FiltrocomboBox.Text != "Todo")
+                {
+                    if (FiltrocomboBox.Text == string.Empty)
+                    {
+                        MessageBox.Show("El filtro no puede estar vacio.");
+                    }
+                    else
+                       if ((string)FiltrocomboBox.Text != "Todo")
                     {
                         if (CriteriotextBox.Text == string.Empty)
                         {
                             MessageBox.Show("Al seleccionar uno de ID, Nombres o Balance necesita escribir algo en el criterio.");
                         }
                     }
+                    listado = r.GetList(p => true);
+                    listado = listado.Where(c => c.FechaIngreso.Date >= DesdedateTimePicker.Value.Date && c.FechaIngreso.Date <= HastadateTimePicker.Value.Date).ToList();
+                }
+                ConsultadataGridView.DataSource = null;
+                ConsultadataGridView.DataSource = listado;
+            }          
+            else
+            {
+                if (CriteriotextBox.Text.Trim().Length > 0)
+                {
+                    switch (FiltrocomboBox.Text)
+                    {
+                        case "Todo":
+                            listado = r.GetList(p => true);
+                            break;
+                        case "ID":
+                            int parse;
+                            if (!int.TryParse(CriteriotextBox.Text, out parse))
+                            {
+                                MessageBox.Show("Solo numeros.");
+                            }
+                            else
+                            {
+                                int id = Convert.ToInt32(CriteriotextBox.Text);
+                                listado = r.GetList(p => p.EstudianteId == id);
+                            }
+                            break;
+                        case "Nombres":
+                            if (!System.Text.RegularExpressions.Regex.IsMatch(CriteriotextBox.Text, "^[a-zA-Z ]"))
+                            {
+                                MessageBox.Show("No numeros en los nombres.");
+                            }
+                            else
+                            {
+                                listado = r.GetList(p => p.Nombres.Contains(CriteriotextBox.Text));
+                            }
+                            break;
+
+                        case "Balance":
+                            double parsec;
+                            if (!double.TryParse(CriteriotextBox.Text, out parsec))
+                            {
+                                MessageBox.Show("Solo numeros.");
+                            }
+                            else
+                            {
+                                double c = double.Parse(CriteriotextBox.Text);
+                                listado = r.GetList(p => p.Balance == c);
+                            }
+                            break;
+                    }
+                }
+                else
+                {
+                    if (FiltrocomboBox.Text == string.Empty)
+                    {
+                        MessageBox.Show("El filtro no puede estar vacio.");
+                    }
+                    else
+                        if ((string)FiltrocomboBox.Text != "Todo")
+                        {
+                            if (CriteriotextBox.Text == string.Empty)
+                            {
+                                MessageBox.Show("Al seleccionar uno de ID, Nombres o Balance necesita escribir algo en el criterio.");
+                            }
+                        }
                     else
                     {
                         listado = r.GetList(p => true);
                     }
-            }
+                }
 
                 ConsultadataGridView.DataSource = null;
                 ConsultadataGridView.DataSource = listado;
+            }
         }
-    }
+    }   
 }
